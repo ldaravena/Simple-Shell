94469424e0dbe12c7b3c032da98fef37a8e7161a
FelipeHR <42549396+FelipeHR@users.noreply.github.com>
2021-10-13 18:08:18 -0300

    Creacion de comandos

--- test.c
+++ test.c
@@ -66,17 +66,33 @@ char* getCommandLine() {
     return line;
 }
 
+int commandExist(char** cmd, int create){// cmd[0] no nos sirve; cmd[1] nombre del nuevo comando; cmd[2] tipo de comando; cmd[3] repeticion cada x s; cmd[4] tiempo total;
+    FILE* archivo;
+    archivo = fopen ("comandos", "r+");
+    if (archivo==NULL) {fputs ("File error",stderr); exit (1);}
+    char* buffer = (char*) malloc(50 * sizeof(char));
+    while(fgets(buffer,50,archivo)!= NULL){
+        char** line = splitLine(buffer);
+        if(cmd[1] != NULL && !strcmp(line[0],cmd[1]) && create) return 1;
+        if(cmd[0] != NULL && !strcmp(line[0],cmd[0]) && !create){
+            execCommand(line);
+            return 1;
+        }
+    }
+    fclose(archivo);
+    return 0;
+}
+
 void launchProgram(char** cmd) {
     if(cmd[0] != NULL){
         if (strcmp(cmd[0], "exit") == 0) exit(0);
         if (strcmp(cmd[0], "cmdmonset") == 0){
-            //Consultamos si el comando existe (una funcion booleana (con int por el maldito c))
+            if(!commandExist(cmd,1)) createCommand(cmd);//Consultamos si el comando existe (una funcion booleana (con int por el maldito c)), y si existe se reemplaza
             //Si es que no existe lo creamos 
             exit(0);
         }
     }
     //Debemos revisar si vamos a ejecutar un comando creado
-
     int pid = fork();
 
     if (pid < 0) {
@@ -98,6 +114,33 @@ void launchProgram(char** cmd) {
     //return;
 }
 
+void execCommand(char** cmd){
+    int rep = atoi(cmd[3])/atoi(cmd[2]);
+    int segundos = atoi(cmd[2]);
+    cmd[0] = cmd[1];
+    cmd[1] = NULL;
+    cmd[2] = NULL;
+    cmd[3] = NULL;
+    for(int i= 0; i<rep; i++){
+        sleep(segundos);
+        launchProgram(cmd);
+    }
+}
+
+
+
+
+void createCommand(char** cmd){
+    FILE* archivo;
+    archivo = fopen ("comandos", "a+");
+    if (archivo==NULL) {fputs ("File error",stderr); exit (1);}
+    printf("%s\n", cmd[2]);
+    fprintf(archivo,"%s %s %s %s\n", cmd[1],cmd[2],cmd[3],cmd[4]);
+    fclose(archivo);
+}
+
+
+
 int main(){
 
 
@@ -113,7 +156,7 @@ int main(){
         char* line = getCommandLine();
 
         char** cmd = splitLine(line);
-        launchProgram(cmd);
+        if(!commandExist(cmd,0)) launchProgram(cmd);
     }
 
 
